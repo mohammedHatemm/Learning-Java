2218eaec788f6b7f129a1b4a43a2c551f29dfc6e
mohamed hatem <m.hatem@crocoit.com>
2026-03-21 14:34:27 +0200

    update main file

--- variable/src/Main.java
+++ variable/src/Main.java
@@ -9,38 +9,38 @@ public class Main {
        int a = 10;
        int b = 20;
 
-//        System.out.println("==========Student Card==========");
-//        System.out.println("Enter your name : ");
-//        Scanner input = new Scanner(System.in);
-//        name = input.nextLine();
-//        System.out.println("Enter your age : ");
-//        age = input.nextInt();
-//        System.out.println("enter your gpa : ");
-//        gpa = input.nextDouble();
-//        input.nextLine(); // مسح الـ buffer
-//        System.out.println("enter your grade : ");
-//        grade = input.nextLine().charAt(0);
-//        System.out.println("is the student graduated :");
-//        isGraduated = gpa >= 2.0;
-//        if (isGraduated)
-//            System.out.println(" yes the student graduated" );
-//
-//        System.out.println("==========Student Card==========");
-//        System.out.println("Name : " + name);
-//        System.out.println("Age : " + age);
-//        System.out.println("GPA : " + gpa);
-//        System.out.println("Grade : " + grade);
-//        System.out.printf("is he graduated ? " + isGraduated);
+        System.out.println("==========Student Card==========");
+        System.out.println("Enter your name : ");
+        Scanner input = new Scanner(System.in);
+        name = input.nextLine();
+        System.out.println("Enter your age : ");
+        age = input.nextInt();
+        System.out.println("enter your gpa : ");
+        gpa = input.nextDouble();
+        input.nextLine(); // مسح الـ buffer
+        System.out.println("enter your grade : ");
+        grade = input.nextLine().charAt(0);
+        System.out.println("is the student graduated :");
+        isGraduated = gpa >= 2.0;
+        if (isGraduated)
+            System.out.println(" yes the student graduated" );
+
+        System.out.println("==========Student Card==========");
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("GPA : " + gpa);
+        System.out.println("Grade : " + grade);
+        System.out.printf("is he graduated ? " + isGraduated);
 
         //### Exercise 2: Variable Swap
 
-//        a = a+b;
-//        b=a-b;
-//        a=a-b;
-//
-//        System.out.println(" the new value of a  is :" + a);
-//        a=b;
-//        System.out.println(" the new value of b  is :" + b);
+        a = a+b;
+        b=a-b;
+        a=a-b;
+
+        System.out.println(" the new value of a  is :" + a);
+        a=b;
+        System.out.println(" the new value of b  is :" + b);
 
 //### Exercise 5: Circle Calculator
         System.out.println("enter the radius of the circle :");
